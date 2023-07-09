d85237bc27b4702c5b71e996f0499159c36bf7b0
Anton Todorov <antontodorov321@gmail.com>
2023-07-09 18:44:25 +0300

    Add add button

---
+++ SoftUni_C#Web_CourseProject/FitnessSite.Web/Controllers/TrainingController.cs
@@ -0,0 +1,12 @@
+﻿namespace FitnessSite.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class TrainingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
